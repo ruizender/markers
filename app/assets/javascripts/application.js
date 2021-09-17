66d3c04efdd52155a8813618af3672b4a80049d3
Ender Ruiz <ruiz.valera@gmail.com>
2021-09-17 14:55:44 -0300

    last commit 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //= require jquery
 //= require chartkick 
-//= require highcharts
+//= require Chart.bundle
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
