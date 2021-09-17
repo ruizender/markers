5614d24d9bfe463dd8c69426b67f6a0fe10e882b
Ender Ruiz <ruiz.valera@gmail.com>
2021-09-16 23:49:32 -0300

    modify file seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+20.times do |i|
+  Category.create(
+    name: Faker::Creature::Animal.name,
+    private: [true,false].sample)
+  Subcategory.create(
+    name: Faker::Creature::Animal.name,
+    category_id:rand(1..19))
+  Marker.create(
+    name: Faker::Creature::Animal.name,
+    url: "https://es.wikipedia.org/wiki/Fauna_de_Venezuela",
+    subcategory_id:rand(1..19))
+  Type.create(
+    name: Faker::Creature::Animal.name,
+    marker_id: rand(1..19))
+end
